258abf0ac5f49130f1670ae0a6c2de11ba6cc7b0
Your_Name Your_Surname <your@email>
2024-01-18 16:29:28 +0200

    registered model Cook on admin.py

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from kitchen.models import Cook, DishType, Dish
+
+
+@admin.register(Cook)
+class CookAdmin(admin.ModelAdmin):
+    list_display = ["year_of_experience", ]
+    fieldsets = (("Additional info", {"fields": ("year_of_experience",)}), )
+    ordering = ["-years_of_experience", ]
+
