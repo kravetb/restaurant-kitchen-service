026e53591d099d14f27fdb27731b9f17f3c115e1
Your_Name Your_Surname <your@email>
2024-01-20 12:02:02 +0200

    add folder templates to TEMPLATES in settings

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from kitchen.views import HomePageView
 
 urlpatterns = [
-    path("home/", HomePageView.as_view(), name="index"),
+    path("", HomePageView.as_view(), name="index"),
 ]
 
 app_name = "kitchen"
--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'restaurant_kitchen_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates", ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
