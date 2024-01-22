b58f3db590245ea080737f040f5ded40dcf1e956
Your_Name Your_Surname <your@email>
2024-01-22 09:37:58 +0200

    Change restaurant_kitchen_service/urls.py and add DishTypeListView to kitchen/views.py and add frontend dish_type_list.html

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from kitchen.views import (
-    HomePageView,
+    HomePageView, DishTypeListView,
 )
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="index"),
+    path("dish-type/", DishTypeListView.as_view(), name="dish-type-list"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -6,3 +6,9 @@ from kitchen.models import DishType
 class HomePageView(generic.base.TemplateView):
     template_name = "kitchen/index.html"
 
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    paginate_by = 5
+    template_name = "kitchen/dish_type_list.html"
+    context_object_name = "dish_type_list"
