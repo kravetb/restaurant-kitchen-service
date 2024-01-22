913256971b29c6f25a90beae9f9277d5b3f27b43
Your_Name Your_Surname <your@email>
2024-01-22 13:01:22 +0200

    Add new content by Home page and add new params to context in HomePageView

--- kitchen/views.py
+++ kitchen/views.py
@@ -6,6 +6,19 @@ from kitchen.models import DishType, Cook, Dish
 class HomePageView(generic.base.TemplateView):
     template_name = "kitchen/index.html"
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        num_cooks = Cook.objects.count()
+        num_dishes = Dish.objects.count()
+        num_dish_types = DishType.objects.count()
+        num_visits = self.request.session.get("num_visits", 0)
+        self.request.session["num_visits"] = num_visits + 1
+        context["num_cooks"] = num_cooks
+        context["num_dishes"] = num_dishes
+        context["num_dish_types"] = num_dish_types
+        context["num_visits"] = num_visits
+        return context
+
 
 class DishTypeListView(generic.ListView):
     model = DishType
