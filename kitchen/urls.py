23db2c5b469517678e3e9b8fe61bc2843f1f4f12
Your_Name Your_Surname <your@email>
2024-01-22 09:21:06 +0200

    Add includes folder to templates and add templatetags to kitchen and add query_transform.py

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from kitchen.views import HomePageView
+from kitchen.views import (
+    HomePageView,
+)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="index"),
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,8 @@
 from django.views import generic
 
+from kitchen.models import DishType
+
 
 class HomePageView(generic.base.TemplateView):
     template_name = "kitchen/index.html"
+
